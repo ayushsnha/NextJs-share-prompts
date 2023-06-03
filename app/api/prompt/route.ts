02727fdd9caeb00b7fd0db5e3dc0ee0d4b14f01d
ayushsnha <ayush_snha@rediffmail.com>
2023-06-03 18:04:21 +0530

    Cache-Control': 'no-store, no-cache, must-revalidate',

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,19 +1,21 @@
 import { connectToDB } from '@utils/database';
-import Prompt from '@models/prompt'
+import Prompt from '@models/prompt';
 
-export const GET = async (request:any) => {
-    try {
-        await connectToDB();
+export const GET = async (request: any) => {
+  try {
+    await connectToDB();
 
-        const prompts = await Prompt.find({}).populate('creator');
+    const prompts = await Prompt.find({}).populate('creator');
 
-        const headers = {
-            'Content-Type': 'application/json',
-            'Cache-Control': 'no-store', // Disable caching
-        };
+    const headers = {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-store, no-cache, must-revalidate',
+      Expires: '0',
+      Pragma: 'no-cache',
+    };
 
-        return new Response(JSON.stringify(prompts), { status: 200, headers });
-    } catch (err) {
-        return new Response("Failed to fetch all Prompts", { status: 500 });
-    }
-}
+    return new Response(JSON.stringify(prompts), { status: 200, headers });
+  } catch (err) {
+    return new Response('Failed to fetch all Prompts', { status: 500 });
+  }
+};
