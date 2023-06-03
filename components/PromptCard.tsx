a4613247267111c7475bc4f5473fd087fd43cde3
ayushsnha <ayush_snha@rediffmail.com>
2023-06-03 16:32:41 +0530

    fetch one data

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -9,6 +9,9 @@ import { usePathname, useRouter } from 'next/navigation';
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }: any) => {
 
     const [copied, setCopied] = useState('');
+    const { data: session }: any = useSession();
+    const pathName = usePathname();
+    const router = useRouter();
 
     const handleCopy = () => {
         setCopied(post.prompt);
@@ -50,6 +53,21 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }: any) => 
             >
                 {post.tag}
             </p>
+            {session?.user.id === post.creator._id &&
+                <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+                    <p className='font-inter text-sm green_gradient cursor-pointer'
+                        onClick={handleEdit}
+                    >
+                        Edit
+                    </p>
+                    <p className='font-inter text-sm orange_gradient cursor-pointer'
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </p>
+                </div>
+
+            }
         </div>
     )
 }
